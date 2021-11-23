1dfa0ae74a6db36e766bb00f75d20d565e30160a
tahminzikrullah <tahminzikrullah@tahminas-mbp.fios-router.home>
2021-11-22 22:04:15 -0500

    Fourth Project Demo commit

---
+++ src/four/DemoFour.java
@@ -0,0 +1,11 @@
+package four;
+
+public class DemoFour {
+	public static void main(String [] args) {
+		//Comment line will also show on GitHub
+		System.out.println("Today is Monday");
+		
+		//Always save class after coding
+	}
+
+}
